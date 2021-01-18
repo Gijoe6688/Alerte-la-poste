ce524c87ec10d46c052e9311161c673a0ebcb048
Gijoe6688 <gijoe6688@gmail.com>
2021-01-18 16:40:46 +0100

    unique

--- src/question2.py
+++ src/question2.py
@@ -1,9 +1,12 @@
 f=open("input/connexion.log","r")
 w=open("output/filtre.txt","w")
+lunique=[]
 for line in f:
     l=line.split(";")
     h=l[-1].split(" ")[-1].strip()
     if h<"08:00" or h>"19:00":
-        w.write(f"{l[1]}; {l[0]}\n")
+        lunique.append(l[1]+"; "+l[0])
+for elt in sorted(set(lunique)):
+    w.write(f"{elt}\n")
 f.close()
 w.close()
