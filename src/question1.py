d2760c7d5a45c43a11f21610db4a43731210b540
Gijoe6688 <gijoe6688@gmail.com>
2021-01-18 15:55:16 +0100

    update

--- src/question1.py
+++ src/question1.py
@@ -1,7 +1,10 @@
 f=open("input/connexion.log","r")
 w=open("output/utilisateurs.txt","w")
+l=[]
 for line in f:
     name=line.split(";")[1]
-    w.write(f"{name}\n")
+    l.append(name)
+for elt in sorted(set(l)):
+    w.write(f"{elt}\n")
 f.close()
 w.close()
